f492c9929db364331850c3caff90625914622da4
chrysn <chrysn@fsfe.org>
2021-02-02 10:12:25 +0100

    pub use heapless
    This is needed by embedded-nal trait implementers who build
    get_host_by_address results, or by trait users who pass the results on.
    
    Closes: https://github.com/rust-embedded-community/embedded-nal/issues/17

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,9 @@ mod dns;
 mod stack;
 
 pub use nb;
+// Needed by embedded-nal trait implementers who build get_host_by_address results, or by trait
+// users who pass the results on.
+pub use heapless;
 pub use no_std_net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6};
 
 pub use dns::{AddrType, Dns};
